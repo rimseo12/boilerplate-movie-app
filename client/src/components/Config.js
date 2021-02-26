6cf44eb76b35a1eef479286df19b78ce49a1c496
rimseo12 <seorimhyun@gmail.com>
2021-02-26 22:02:17 +0900

    메인 화면 이미지랑 관련 정보 불러오기

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '144110d2dc915cd6ac7194a2bc8b778d';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
