89cbbb95585ce2696690ef976c8ddebe8dd55834
rimseo12 <seorimhyun@gmail.com>
2021-02-27 21:28:34 +0900

    mogoDB 연결 오류 해결, GridCard Component 생성
    TypeError: Cannot read property 'prototype' of undefined  -->오류 발생시 import issue였다.

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,4 +3,4 @@ export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = '144110d2dc915cd6ac7194a2bc8b778d';
-export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,38 +1,57 @@
-import { response } from 'express';
 import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCard from '../commons/GridCard';
+import { Row } from 'antd';
 
 function LandingPage() {
-    const [Movies, setMovies] = useState([])
+    const [Movies, setMovies] = useState()
     const [MainMovieImage, setMainMovieImage] = useState(null)
+
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
-                setMovies([response.results])
+
+                console.log(response)
+
+                setMovies(...[response.results])
                 setMainMovieImage(response.results[0])
             })
+
     }, [])
     return (
         <div style={{ width: '100%', margin: '0' }}>
             {/* 메인 이미지 */}
             {MainMovieImage &&
-                <MainImage 
-                image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
-                title={MainMovieImage.original_title}
-                text={MainMovieImage.overview}
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
                 />
             }
 
-            <div style={{ width: '85%', margin: '1 rem auto' }}>
+            <div style={{ width: '80%', margin: '1rem auto' }}>
                 <h2>Movies by latest</h2>
                 <hr />
 
                 {/* 그리드 카드 */}
+                <Row gutter={[16, 16]}>
+                    {Movies && Movies.map((movie, index) => (
+                        <React.Fragment key={index}>
+                            <GridCard
+                                image={movie.poster_path ?
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
 
             </div>
 
