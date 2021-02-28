ceb16988038dc8a588680fdf064829ba547ce65b
rimseo12 <seorimhyun@gmail.com>
2021-02-28 17:25:45 +0900

    loadMore event 구현
    어느 정도 간격을 둔 상태에서 로딩하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,28 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { FaCode } from "react-icons/fa";
-import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
-import MainImage from './Sections/MainImage';
-import GridCard from '../commons/GridCard';
-import { Row } from 'antd';
+import React, { useEffect, useState, useRef } from 'react'
+import { FaCode } from "react-icons/fa"
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config'
+import MainImage from './Sections/MainImage'
+import GridCard from '../commons/GridCard'
+import { Row } from 'antd'
+
 
 function LandingPage() {
-    const [Movies, setMovies] = useState()
+    const buttonRef = useRef(null)
+    const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [Loading, setLoading] = useState(true)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetchMovies(endpoint)
+    }, [])
+    
+    //자동 스크롤 이벤트 호출
+    useEffect(() => {
+        function onScroll () {
+            if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300) {
+                buttonRef.current.click()
+            }
+        }
 
+        window.addEventListener("scroll", onScroll)
+        return () => {
+            window.removeEventListener("scroll", onScroll)
+        }     
+    }, [])
+
+    //common fetch code
+    const fetchMovies = (endpoint) => {
         fetch(endpoint)
             .then(response => response.json())
             .then(response => {
-
-                console.log(response)
-
-                setMovies(...[response.results])
+                setMovies([...Movies, ...response.results])
                 setMainMovieImage(response.results[0])
-            })
+                setCurrentPage(response.page)
+            }, setLoading(false))
+            .catch(error => console.error('Error:', error))
+    }
 
-    }, [])
+    const loadMoreItems = () => {
+        setLoading(true)
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`
+        fetchMovies(endpoint)
+    }
+
+   
     return (
         <div style={{ width: '100%', margin: '0' }}>
             {/* 메인 이미지 */}
@@ -47,7 +75,6 @@ function LandingPage() {
                                     `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                                 movieId={movie.id}
                                 movieName={movie.original_title}
-
                             />
                         </React.Fragment>
                     ))}
@@ -56,7 +83,7 @@ function LandingPage() {
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <button>Load More</button>
+                <button ref={buttonRef} className="loadMore" onClick={loadMoreItems}>Load More</button>
             </div>
         </div>
     )
